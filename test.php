e8d56aa67d6cda818e08a0bfc62a4bf89630f8b0
Georgi Georgiev <georgi.georgiev.vn@gmail.com>
2014-05-06 02:54:51 +0300

    Some changes and create 1.3 tag version

--- test.php
+++ test.php
@@ -9,3 +9,4 @@ echo 'медала';
 echo 'mi tuka ot linux-a';
 echo 'Add some functionality for release v1.1';
 echo 'Add some new modules and create new release v1.2';
+echo 'Add some modules and create release v1.3';
