d84414a23090d6078a8de483e1387ca00e6e4fe5
Georgi Georgiev <torque_vn@yahoo.com>
2014-05-06 03:44:24 +0300

    alata balata

--- test.php
+++ test.php
@@ -9,3 +9,4 @@ echo 'медала';
 echo 'mi tuka ot linux-a';
 echo 'Add some functionality for release v1.1';
 echo 'Add some new modules and create new release v1.2';
+echo 'Some change for 1.5 release version';
