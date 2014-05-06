faee751d45683037d3178a3cf7a4ee2f965ca897
Georgi Georgiev <georgi.georgiev.vn@gmail.com>
2014-05-06 03:37:47 +0300

    Some commit message for realease v1.3 i think ..

--- test.php
+++ test.php
@@ -10,3 +10,4 @@ echo 'mi tuka ot linux-a';
 echo 'Add some functionality for release v1.1';
 echo 'Add some new modules and create new release v1.2';
 echo 'Add some modules and create release v1.3';
+echo 'Additions and create new release v1.4';
